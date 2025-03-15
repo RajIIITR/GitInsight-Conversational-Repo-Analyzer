7e39e29a790d2550d9a4ad95892a26de8373dbcc
RajIIITR <101034828+RajIIITR@users.noreply.github.com>
2025-03-15 14:07:06 +0530

    Delete src/__init__.py

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import streamlit as st
-import google.generativeai as genai
-from youtube_transcript_api import YouTubeTranscriptApi
-from dotenv import load_dotenv
-
-# Initialize environment variables
-load_dotenv()
